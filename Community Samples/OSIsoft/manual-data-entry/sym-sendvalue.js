60bb1254488a8fbe80a9c0d7297adc4021c37994
AnnaPerry <anutamperry@gmail.com>
2017-02-21 15:32:08 -0600

    Update sym-sendvalue.js

--- Community Samples/OSIsoft/manual-data-entry/sym-sendvalue.js
+++ Community Samples/OSIsoft/manual-data-entry/sym-sendvalue.js
@@ -9,7 +9,14 @@
 	    		DataShape: 'TimeSeries',
 		        Height: 50,
 		        Width: 450,
-				defaultTimestamp: '*'
+				defaultTimestamp: '*',
+				bgColor: "#239a23",
+				textColor: "#fff",
+				btnWidth: "80px",
+				btnHeight: "26px",
+				btnText: "Update",
+				showTimestamp: false,
+				valColWidth: "150px"
 			};
 		},
 	    configOptions: function () {
@@ -26,8 +33,7 @@
 	CS.deriveVisualizationFromBase(symbolVis);
 		
 	symbolVis.prototype.init = function (scope, elem, $http){
-	console.log("SCOPE");
-		console.log(scope);
+
 		
 		
 		this.onDataUpdate = dataUpdate;
@@ -35,14 +41,18 @@
 		function dataUpdate(data) {
 		
 	        if(data) {
+			
 	            if(data.Data[0].Label) {
 					//get Data Streams configuration from the data object provided by PI Coresight data layer
 					scope.dataStreamList = data.Data;
-        			console.log(scope.dataStreamList);
+					//console.log("Logging scope.symbol.Name");
+					//console.log(scope.symbol.Name);
+        			//console.log("Logging scope.$id");
+					//console.log(CS);
 	            }
 	        }
 	    };
-		
+		// TODO: Replace dataStreamList with:
 		//		scope.dataStreamList = scope.symbol.DataSources;
 		scope.isStreamSelectedList = {};
 		scope.config.submittedData = {};
@@ -51,28 +61,31 @@
 		scope.areAnyStreamsSelected = false;
 
 		
-		var baseUrl = CS.ClientSettings.PIWebAPIUrl.replace(/\/?$/, '/'); //Example: "https://example.com/piwebapi/";
-		console.log("baseUrl");
-		console.log(baseUrl);
-		
+		var baseUrl = CS.ClientSettings.PIWebAPIUrl.replace(/\/?$/, '/'); //Example: "https://arcadia.osisoft.int/piwebapi/";
+
 		scope.inputType = {};
 		scope.isEnumerationType = {};
 		scope.enumerationOptions= {};
 		var TYPES = {
 			Single: "Number",
 			Double: "Number",
+			Float16: "Number", 
+			Float32: "Number",
+			Float64: "Number",
 			Int16: "Number",
 			Int32: "Number",
 			Int64: "Number",
 			String: "String",
 			EnumerationValue: "String", //String for now, but should be handled specially
-			Boolean: "Boolean"
+			Boolean: "Boolean",
+			DateTime: "String"
 			
 		};
 		
 		
 	   scope.sendValue = function(){
 		   
+		   scope.config.loading = true;
 		    //Send data only if we have the following:
 			//scope.config.submittedData - WHAT to send 
 			//dataStreamList - WHERE to send
@@ -82,7 +95,7 @@
 				//Form a batch request to send to PI Web API
 				//TODO = example of batch request
 				var batchRequest = formBulkSendRequest(scope.dataStreamList,scope.config.submittedData);
-				console.log(batchRequest);
+				//console.log(batchRequest);
 				
 				//Send batch request to PI Web API endpoint
 				$http({
@@ -93,6 +106,11 @@
 					headers: {
 						'Content-Type': 'application/json',
 					}
+				}).success(function(response){
+					setTimeout(function() {
+						scope.config.loading = false;
+					}, 2000);
+					
 				});     
      
                 
@@ -107,6 +125,7 @@
 				scope.isStreamSelectedList[index] = toggleValue;
 			});
 			
+			scope.areAnyStreamsSelected  = anyStreamsSelected();
 		};
 		
 		scope.cbSelectionUpdate = function(){
@@ -125,16 +144,17 @@
 			{
 				var itemPath = scope.dataStreamList[index].Path;
 				
-				var getAttributesURL = baseUrl + "attributes?path=" + itemPath;
+				var isAttribute = itemPath.includes("|") ? true : false;
+				var getDataStreamURL = isAttribute ? encodeURI(baseUrl + "attributes?path=" + itemPath) : encodeURI(baseUrl + "points?path=" + itemPath);
 				
-				
-				$http.get(getAttributesURL).success(function(response){
+				$http.get(getDataStreamURL, {withCredentials: true}).success(function(response){
+						
 						//General case
-						scope.inputType[index] = TYPES[response.Type];
+						scope.inputType[index] = isAttribute ? TYPES[response.Type] : TYPES[response.PointType];
 						
 						
 						//Attribute with Enumeration Set Value type case
-						if (response.Type == "EnumerationValue")
+						if (isAttribute && response.Type == "EnumerationValue")
 						{	
 							scope.isEnumerationType[index] = true; 
 													
@@ -206,8 +226,8 @@
 
 					var itemPath = dataStream.Path;
 					
-					
-					var streamInfoUrl = itemPath.includes("|") ? encodeURI(baseUrl + "attributes?path=" + itemPath + "&selectedFields=Links.Value") : encodeURI(baseUrl + "points?path=" + itemPath + "&selectedFields=Links.Value");
+					var isAttribute = itemPath.includes("|") ? true : false;
+					var streamInfoUrl = isAttribute ? encodeURI(baseUrl + "attributes?path=" + itemPath + "&selectedFields=Links.Value") : encodeURI(baseUrl + "points?path=" + itemPath + "&selectedFields=Links.Value");
 					
 					
 					var data = {
@@ -215,12 +235,29 @@
                         "Value": scope.isEnumerationType[index] ? submittedData.values[index].Name : submittedData.values[index]
 					};
 					
+					//var method = isAttribute ? "POST"
+					
 					batchRequest["GetStreamInfo" + index.toString()] = {
 						"Method": "GET",
 						"Resource": streamInfoUrl
 					}
 					batchRequest["PostValue" + index.toString()] = {
 								"Method": "POST",
+								"Resource": "{0}",
+								"Content": JSON.stringify(data),
+								"Headers": {
+									'Content-Type': 'application/json'
+								},
+								"ParentIds": [
+									"GetStreamInfo" + index.toString()
+								],
+								"Parameters": [
+									"$.GetStreamInfo" + index.toString() + ".Content.Links.Value"
+								]
+						
+					}
+					batchRequest["PutValue" + index.toString()] = {
+								"Method": "PUT",
 								"Resource": "{0}",
 								"Content": JSON.stringify(data),
 								"Headers": {
@@ -245,7 +282,7 @@
 			disabled: {
 				'cursor': 'not-allowed',
 				'opacity':'0.65',
-				'background-color': '#239a23',
+				'background-color': scope.config.bgColor,
 				'border': '1px solid rgba(230,231,232,.55)',
 				'color': '#fff',
 				'width': '80px',
@@ -254,7 +291,7 @@
 				'margin-left': 'auto'
 				},
 			general: {
-				'background-color': '#239a23',
+				'background-color': scope.config.bgColor,
 				'border': '1px solid rgba(230,231,232,.55)',
 				'color': '#fff',
 				'width': '80px',
