f34d43a6cf26aae6fcd6281742d0f7e6adcd35a0
AnnaPerry <anutamperry@gmail.com>
2018-07-11 23:45:28 -0500

    Removed hardcoded parts

--- Community Samples/OSIsoft/gantt/sym-ganttchart.js
+++ Community Samples/OSIsoft/gantt/sym-ganttchart.js
@@ -7,14 +7,22 @@
 	var definition = {
 		typeName: "ganttchart",
 		visObjectType: symbolVis,
-		datasourceBehavior: PV.Extensibility.Enums.DatasourceBehaviors.Multiple,
+		datasourceBehavior: PV.Extensibility.Enums.DatasourceBehaviors.Single,
 		getDefaultConfig: function(){
 			return {
 				DataShape: "Timeseries",
 				Height: 150,
-				Width: 150
+				Width: 400,
+				colors: []
 			}		
-		}
+		},
+		configOptions: function () {
+            return [{
+				title: 'Format Symbol',
+                mode: 'default'
+            }];
+        },
+		inject: [ 'webServices' ]
 	}
 	
 	function getConfig() {
@@ -30,13 +38,11 @@
 			"minPeriod": "ss",
 			"ignoreAxisWidth": true
 		  },
-		  //"brightnessStep": 7,
 		  "graph": {
-			//"fillAlphas": 1,
 			"lineAlpha": 1,
 			"lineColor": "#fff",
 			"fillAlphas": 0.85,
-			"balloonText": "<br />[[start]] -- [[end]]"
+			"balloonText": "[[state]]:<br />[[open]] -- [[value]]"
 		  },
 		  "rotate": true,
 		  "categoryField": "category",
@@ -45,9 +51,6 @@
 		  "startDateField": "start",
 		  "endDateField": "end",
 		  "dataProvider": "",
-/* 		  "valueScrollbar": {
-			"autoGridCount": true
-		  }, */
 		  "chartCursor": {
 			"cursorColor": "#55bb76",
 			"valueBalloonsEnabled": false,
@@ -64,62 +67,68 @@
 		}
 	}
 	
-	symbolVis.prototype.init = function(scope, elem) {
+	var TRACECOLORS = ["rgb(62, 152, 211)", "rgb(224, 138, 0)", "rgb(178, 107, 255)", "rgb(47, 188, 184)", "rgb(219, 70, 70)", "rgb(156, 128, 110)", "rgb(60, 191, 60)", "rgb(197, 86, 13)","rgb(46, 32, 238)","rgb(165, 32, 86)" ];
+	
+	symbolVis.prototype.init = function(scope, elem, webServices) {
 		var symbolContainerDiv = elem.find("#container")[0];
 		symbolContainerDiv.id = "barChart_" + Math.random().toString(36).substr(2, 16);
 		var chartConfig = getConfig();
-		console.log(chartConfig);
-		var chart = AmCharts.makeChart(symbolContainerDiv.id, chartConfig);
-		var labels;
-		this.onDataUpdate = dataUpdate;
 		
+		var states = [];
+		webServices.getDefaultStates(scope.symbol.DataSources[0]).promise.then(function(response){
+			states = response.data.States;
+			if (scope.config.states != states) {
+				scope.config.states = states;
+			}
+			if (scope.config.colors.length == 0) {
+				scope.config.colors = states.map(function(item, index){
+					 return TRACECOLORS[index] || index - 1 - (TRACECOLORS.length * Math.floor(index / TRACECOLORS.length));
+				});
+			}
+			
+			if(scope.config.colors.length < states.length) {
+				var iterations = states.length - scope.config.colors.length;
+				for (var i = 0; i < iterations; i++) {
+					scope.config.colors.push('#'+Math.floor(Math.random()*16777215).toString(16));
+				}
+			}
+		});
+		
+		var chart;
+		this.onDataUpdate = dataUpdate;
+
 		function dataUpdate(data) {
-			if(!data) return;
-			var dataprovider = convertToChartDataFormat(data);
-			chart.dataProvider = dataprovider;
+			if(!data || !states.length > 0) return;
+			if(!chart) { chart = AmCharts.makeChart(symbolContainerDiv.id, chartConfig) }
+			
+			chart.dataProvider = convertToChartDataFormat(data);
 			chart.validateData();
 		}
 		
 		function convertToChartDataFormat(data) {
-			var valArray = data.Data[0].Values;
-			var segmentsRunning = [];
-			var segmentsDown = [];
-			var segmentsTrouble = [];
+			var valArray = data.Data[0].Values;	
+			var segments = [];
+			
 			valArray.forEach(function(item, index) {
-				switch(item.Value) {
-					case "Running":
-						segmentsRunning.push({
-							"start": new Date(item.Time),
-							"end": index + 1 >= valArray.length ? new Date(data.Data[0].EndTime) : new Date(valArray[index + 1].Time)
-						})
-						break;
-					case "Down":
-						segmentsDown.push({
-							"start": new Date(item.Time),
-							"end": index + 1 >= valArray.length ? new Date(data.Data[0].EndTime) : new Date(valArray[index + 1].Time)
-						})
-						break;
-					case "Trouble":
-						segmentsTrouble.push({
-							"start": new Date(item.Time),
-							"end": index + 1 >= valArray.length ? new Date(data.Data[0].EndTime) : new Date(valArray[index + 1].Time)
-						})
-						break;
-					default:
-						break;
+				
+				var colorIndex = states.findIndex(function(state){ return state.Name == item.Value});
+				if (valArray[index - 1] && item.Value == valArray[index - 1].Value) {
+					segments[segments.length - 1].end = index == valArray.length - 1 ? new Date(data.Data[0].EndTime) : new Date(valArray[index + 1].Time);
+					
+				} else {
+						segments.push({
+						"start": new Date(item.Time),
+						"end": index == valArray.length - 1 ? new Date(data.Data[0].EndTime) : new Date(valArray[index + 1].Time),
+						"color": scope.config.colors[colorIndex],
+						"state": item.Value
+					})
 				}
 			});
-		 var dataprovider = [{
-			"category": "Running",
-			"segments": segmentsRunning
-		  }, {
-			"category": "Down",
-			"segments": segmentsDown
-		  }, {
-			"category": "Trouble",
-			"segments": segmentsTrouble
-		  }];
-		 return dataprovider;
+			
+			return [{
+				"category": "",
+				"segments": segments
+			}];
 		}
 	}
 
